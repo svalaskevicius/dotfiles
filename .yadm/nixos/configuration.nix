030b0ddf969d5d099a42808a427a51102706387a
Sarunas Valaskevicius <rakatan@gmail.com>
2018-08-12 17:10:07 +0100

    add new laptop

--- .yadm/nixos/configuration.nix
+++ .yadm/nixos/configuration.nix
@@ -8,37 +8,21 @@
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      ./local.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices = [
-    {
-      name = "root";
-      device = "/dev/disk/by-uuid/b4da34ca-073f-4e3b-acc3-f90bf082f6af";
-      preLVM = true;
-      allowDiscards = true;
-    }
-  ];
-
   # Supposedly better for the SSD.
   fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
 
-  networking.hostName = "tx-saru-nix"; # Define your hostname.
   networking.hosts = {
     "127.0.0.1" = [config.networking.hostName];
   };
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "latarcyrheb-sun32";
-    consoleKeyMap = "uk";
-    defaultLocale = "en_GB.UTF-8";
-  };
 
   # Set your time zone.
   time.timeZone = "Europe/London";
@@ -96,7 +80,6 @@
   # services.printing.enable = true;
 
   fonts.fontconfig.enable = true;
-  fonts.fontconfig.dpi = 192;
   fonts.fontconfig.antialias = true;
 
   # Enable sound.
@@ -107,7 +90,6 @@
     acpid.enable = true;
     xserver = {
       enable = true;
-      dpi = 192;
       layout = "gb";
       # xkbOptions = "eurosign:e";
 
