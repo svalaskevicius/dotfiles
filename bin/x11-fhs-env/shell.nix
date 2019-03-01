54e0a24f87a57378d70c3950b4db17190b122305
Sarunas Valaskevicius <rakatan@gmail.com>
2019-03-01 21:54:30 +0000

    run idea in it's own nix environment

---
+++ bin/x11-fhs-env/shell.nix
@@ -0,0 +1,48 @@
+{
+  pkgs ? import <nixpkgs> {}
+  , run ? "bash"
+}:
+
+(pkgs.buildFHSUserEnv {
+  name = "simple-x11-env";
+  targetPkgs = pkgs: (with pkgs;
+    [ udev
+      alsaLib
+      which
+      stdenv.cc.libc
+      glib
+      libxml2
+      libav_0_8
+      ffmpeg
+      libxslt
+      libGL
+      alsaLib
+      fontconfig
+      freetype
+      pango
+      gtk2
+      cairo
+      gdk_pixbuf
+      atk
+
+      git
+      openjdk8 maven scala sbt gnumake gcc nodejs
+      nerdfonts fira-code
+    ]) ++ (with pkgs.xorg;
+    [ libX11
+      libXcursor
+      libXrandr
+      libXext
+      libXtst
+      libXi
+      libXp
+      libXt
+      libXrender
+      libXxf86vm
+    ]);
+  multiPkgs = pkgs: (with pkgs;
+    [ udev
+      alsaLib
+    ]);
+  runScript = run;
+}).env
