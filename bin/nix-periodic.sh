a7387e4c27c73f1b17a0ee3d27a23100dfd3527f
Sarunas Valaskevicius <rakatan@gmail.com>
2019-05-12 17:54:39 +0100

    fstrim after cleanup

--- bin/nix-periodic.sh
+++ bin/nix-periodic.sh
@@ -3,6 +3,7 @@
 set -x -e
 
 sudo nix-collect-garbage -d --delete-older-than 14d
+sudo fstrim -v -a
 sudo nix-channel --update nixos
 sudo nix-channel --update nixos-unstable
 sudo nixos-rebuild switch --upgrade
