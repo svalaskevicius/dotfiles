51310bd8529e6150fc385d71e9b3741b6973f94b
Sarunas Valaskevicius <rakatan@gmail.com>
2022-05-31 16:11:17 +0100

    updates

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -66,7 +66,7 @@ require('packer').startup(function(use)
     requires = { 'nvim-lua/plenary.nvim' }
   }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
+  use { 'akinsho/bufferline.nvim', tag = "*", requires = 'kyazdani42/nvim-web-devicons' }
   use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons'} }
   use { 'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'} }
   -- use {
@@ -85,8 +85,12 @@ require('packer').startup(function(use)
   use 'sindrets/winshift.nvim'
   -- use 'mfussenegger/nvim-jdtls'
 
-  use({ "sheerun/vim-polyglot" })
+  use 'sheerun/vim-polyglot'
+  use 'ggandor/leap.nvim'
+  -- use 'vim-scripts/AnsiEsc.vim'
 end)
+
+require('leap').set_default_keymaps()
 
 require'lspconfig'.jdtls.setup{
   cmd = { '/home/sarunas/src/eclipse.jdt.ls/run.sh' }
@@ -113,7 +117,7 @@ g['nvim_web_devicons'] = 1 -- temporary until nvim-tree removes check?
 -- VARIABLES ---------------------
 ----------------------------------
 -- nvim-metals
-g['metals_server_version'] = '0.11.1+28-f3d65d76-SNAPSHOT'
+g['metals_server_version'] = '0.11.5+157-d0cf15ce-SNAPSHOT'
 
 ----------------------------------
 -- OPTIONS -----------------------
@@ -125,15 +129,12 @@ vim.o.shortmess = string.gsub(vim.o.shortmess, 'F', '') .. 'c'
 -- nvim tree
 
 g['nvim_tree_auto_ignore_ft'] = {} -- [ 'startify', 'dashboard' ] -- empty by default, don't auto open tree on specific filetypes.
-g['nvim_tree_quit_on_open'] = 0 -- 0 by default, closes the tree when you open a file
-g['nvim_tree_indent_markers'] = 1 -- 0 by default, this option shows indent markers when folders are open
 g['nvim_tree_git_hl'] = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 g['nvim_tree_highlight_opened_files'] = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 g['nvim_tree_root_folder_modifier'] = ':~' -- This is the default. See :help filename-modifiers for more options
 g['nvim_tree_width_allow_resize']  = 1 -- 0 by default, will not resize the tree when opening a file
 g['nvim_tree_add_trailing'] = 1 -- 0 by default, append a trailing slash to folder names
 g['nvim_tree_group_empty'] = 1 --  0 by default, compact folders that only contain a single folder into one node in the file tree
-g['nvim_tree_disable_window_picker'] = 0 -- 0 by default, will disable the window picker.
 g['nvim_tree_special_files'] = { 'README.md', 'Makefile', 'MAKEFILE' } --  List of filenames that gets highlighted with NvimTreeSpecialFile
 g['nvim_tree_show_icons'] = { git = 1, folders = 1, files = 1 }
 
@@ -143,6 +144,14 @@ map('n', '<leader>tf', '<cmd>NvimTreeFindFile<CR>')
 
 
 require'nvim-tree'.setup({
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = true
+      },
+      quit_on_open = false
+    }
+  },
     -- disables netrw completely
   disable_netrw       = true,
   -- hijack netrw window on startup
@@ -151,15 +160,13 @@ require'nvim-tree'.setup({
   open_on_setup       = false,
   -- will not open on setup if the filetype is in this list
   ignore_ft_on_setup  = {},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close          = true,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab         = false,
   -- hijacks new directory buffers when they are opened.
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
+  -- update_to_buf_dir   = {
+  --   enable = true,
+  --   auto_open = true,
+  -- },
   -- hijack the cursor in the tree to put it at the start of the filename
   hijack_cursor       = false,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
@@ -193,7 +200,7 @@ require'nvim-tree'.setup({
     -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
     side = 'left',
     -- if true the tree will resize itself after opening a file
-    auto_resize = false,
+    -- auto_resize = false,
     mappings = {
       -- custom only false will merge the list with the default mappings
       -- if true, it will only use your list to set the mappings
@@ -211,6 +218,11 @@ require'nvim-tree'.setup({
     ignore = true,
     timeout = 500,
   },
+  renderer = {
+    indent_markers = {
+      enable = true
+    }
+  }
 })
 
 -- LSP
