e38399bee3b75b4b3c9da6c8022fea17165f216a
Sarunas Valaskevicius <rakatan@gmail.com>
2025-11-15 13:35:50 +0000

    re-enable minuet.ai

--- .config/nvim/lua/fredrik/plugins/core/lualine.lua
+++ .config/nvim/lua/fredrik/plugins/core/lualine.lua
@@ -68,14 +68,14 @@ return {
       },
       lualine_x = {
         {
-          -- require 'minuet.lualine',
+          require 'minuet.lualine',
           -- the follwing is the default configuration
           -- the name displayed in the lualine. Set to "provider", "model" or "both"
-          -- display_name = 'both',
+          display_name = 'model',
           -- separator between provider and model name for option "both"
-          -- provider_model_separator = ':',
+          provider_model_separator = ':',
           -- whether show display_name when no completion requests are active
-          -- display_on_idle = false,
+          display_on_idle = false,
         },
         macro_recording,
         "encoding",
